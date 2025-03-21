f60cb02542f1c635e95e11015403569cef442300
Grey Wind <wjunqin7@gmail.com>
2025-03-21 23:09:12 +0800

    add simple method about single thread download class

--- QingYi.Core/Network/Download/Downloader.cs
+++ QingYi.Core/Network/Download/Downloader.cs
@@ -4,6 +4,17 @@ namespace QingYi.Core.Network.Download
 {
     public class Downloader
     {
+        public static void Download(string url, string savePath, string fileName, DownloadType downloadType = DownloadType.SingleThread) => DownloadCommon(url, savePath, fileName, 81920, downloadType);
+
+        public static void Download(string url, string savePath, string fileName, int bufferSize = 81920, DownloadType downloadType = DownloadType.SingleThread) => DownloadCommon(url, savePath, fileName, bufferSize, downloadType);
+
+        private static void DownloadCommon(string url, string savePath, string fileName, int bufferSize, DownloadType downloadType = DownloadType.SingleThread)
+        {
+            if (downloadType == DownloadType.SingleThread)
+            {
+                SingleThreadDownload.Download(url, savePath, fileName, bufferSize);
+            }
+        }
     }
 
     [Flags]
