889217a6ee9d5cfc175bc17bf7e7482d28ea2aad
Grey Wind <wjunqin7@gmail.com>
2025-03-29 14:22:02 +0800

    Base 56 test

--- Test/BaseTest/Program.cs
+++ Test/BaseTest/Program.cs
@@ -196,6 +196,25 @@ namespace BaseTest
             GC.Collect();
             #endregion
 
+            #region Base56
+            // 设置前景颜色
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.Write("·Base56 ");
+
+            // 设置前景颜色
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Encode: ");
+            Console.Write($"{Base56.EncodeString(testText, StringEncoding.UTF8)}  ");
+            Console.Write("Decode: ");
+            Console.Write($"{Base56.DecodeString(Base56.EncodeString(testText, StringEncoding.UTF8), StringEncoding.UTF8)}\n");
+
+            // 恢复为默认颜色
+            Console.ResetColor();
+
+            GC.Collect();
+            #endregion
+
             #region Base58
             #endregion
 
